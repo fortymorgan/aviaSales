aea133067e42ea267a24c6b189034be63b198573
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-17 21:09:21 +0300

    Change custom display of date and time to moment.js formatting

--- aviasales.js
+++ aviasales.js
@@ -54,47 +54,12 @@ const fillFilter = () => {
   });
 };
 
-const dateInFormat = dateString => {
-  const dateArray = dateString.split('.');
-  const date = new Date(`20${dateArray[2]}`, dateArray[1], dateArray[0]);
-  const day = date.getDate();
-  const daysOfWeek = {
-    0: 'Вс',
-    1: 'Пн',
-    2: 'Вт',
-    3: 'Ср',
-    4: 'Чт',
-    5: 'Пт',
-    6: 'Сб',
-  };
-  const dayOfWeek = daysOfWeek[date.getDay()];
-  const months = {
-    1: 'янв',
-    2: 'фев',
-    3: 'мар',
-    4: 'апр',
-    5: 'мая',
-    6: 'июня',
-    7: 'июля',
-    8: 'авг',
-    9: 'сен',
-    10: 'окт',
-    11: 'ноя',
-    12: 'дек',
-  };
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
-  return `${day} ${month} ${year},${dayOfWeek}`;
-};
-
 const airLogos = {
   TK: 'logos/TK.png',
   S7: 'logos/S7.png',
   BA: 'logos/BA.png',
   SU: 'logos/SU.png',
 };
-
-const fullTimeString = timeString => (timeString.length < 5 ? `0${timeString}`: timeString)
 
 const render = () => {
   toggleAllCheckbox.checked = [...document.querySelectorAll('input[id^=stops]')]
@@ -118,20 +83,20 @@ const render = () => {
     </div>
     <div class="ticket-info">
       <div class="time-stops">
-        <div class="departure-time">${fullTimeString(ticket.departure_time)}</div>
+        <div class="departure-time">${moment(ticket.departure_time, 'HH:mm').format('HH:mm')}</div>
         <div class="stops">
           <div class="stops-count">${stopsString}</div>
         </div>
-        <div class="arrival-time">${fullTimeString(ticket.arrival_time)}</div>
+        <div class="arrival-time">${moment(ticket.arrival_time, 'HH:mm').format('HH:mm')}</div>
       </div>
       <div class="point-date">
         <div class="departure-point-date">
           <div class="departure-point">${ticket.origin}, ${ticket.origin_name}</div>
-          <div class="departure-date">${dateInFormat(ticket.departure_date)}</div>
+          <div class="departure-date">${moment(ticket.departure_date, 'DD.MM.YY').locale('ru').format('DD MMM YYYY,dd')}</div>
         </div>
         <div class="arrival-point-date">
           <div class="arrival-point">${ticket.destination_name}, ${ticket.destination}</div>
-          <div class="arrival-date">${dateInFormat(ticket.arrival_date)}</div>
+          <div class="arrival-date">${moment(ticket.arrival_date, 'DD.MM.YY').locale('ru').format('DD MMM YYYY,dd')}</div>
         </div>
       </div>
     </div>`;
